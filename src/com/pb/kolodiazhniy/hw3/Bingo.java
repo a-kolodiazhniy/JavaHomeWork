7465e5f23aa5efa6cd8ee49ae2fa77a3b78c6e8a
Aleksandr Kolodiazhniy <kolodiazhniy.aleksandr@privatbank.ua>
2021-10-19 17:55:11 +0300

    hw3

---
+++ src/com/pb/kolodiazhniy/hw3/Bingo.java
@@ -0,0 +1,39 @@
+package com.pb.kolodiazhniy.hw3;
+
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+        int from = 0, to = 100;
+        int NUMBER = 33;
+        int tries = 0;
+        String exit = "exit";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Угадайте число");
+
+        while (true){
+            System.out.println("Введите целое число или exit");
+            String input = in.next();
+            if (input.equals(exit)) break;
+
+            try {
+                int num = Integer.parseInt(input);
+                if (num < from || num > to) {
+                    System.out.println("Число должно быть в диапазоне [" + from + ".." + to + "]\n");
+                    continue;
+                }
+                tries++;
+                if (num == NUMBER) {
+                    System.out.println("Вы угадали число за " + tries + " попыток\n");
+                    break;
+                }else if (num > NUMBER) {
+                    System.out.println("Указанное число больше загаданного\n");
+                }else {
+                    System.out.println("Указанное число меньше загаданного\n");
+                }
+            }catch(Exception ignored) {}
+        }
+
+        System.out.println("Конец игры");
+    }
+}
